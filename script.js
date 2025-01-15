7f1190b3a58f827ad9493fa29353cc44ebcc773f
Fernando Moreno <fermoipn2.98@gmail.com>
2025-01-15 03:43:43 -0600

    feat: Funcion Solicitar CambiarNombre

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function solicitarYCambiarNombre (){
+function solicitarYCambiarNombre() {
     const nombre = prompt("¿Cuál es tu nombre?");
     // uso de la api DOM para cambiar el texto de un elemento
     document.getElementById("texto-saludo").innerText = "Hola " + nombre;
-  
-  }
+
+}
